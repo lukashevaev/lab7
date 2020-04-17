8eb782d0442b088691a8dc334fa50cbf988761af
lukashevaev <46032891+lukashevaev@users.noreply.github.com>
2020-04-17 10:42:56 +0400

    Add files via upload

---
+++ button/button/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+    namespace Button
+    {
+        partial class Form1
+        {
+            /// <summary>
+            /// Required designer variable.
+            /// </summary>
+            private System.ComponentModel.IContainer components = null;
+
+            /// <summary>
+            /// Clean up any resources being used.
+            /// </summary>
+            /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+            protected override void Dispose(bool disposing)
+            {
+                if (disposing && (components != null))
+                {
+                    components.Dispose();
+                }
+                base.Dispose(disposing);
+            }
+
+            #region Windows Form Designer generated code
+
+            /// <summary>
+            /// Required method for Designer support - do not modify
+            /// the contents of this method with the code editor.
+            /// </summary>
+            private void InitializeComponent()
+            {
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button.Location = new System.Drawing.Point(145, 132);
+            this.button.Margin = new System.Windows.Forms.Padding(10);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(137, 52);
+            this.button.TabIndex = 0;
+            this.button.Text = "Push me";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.OnClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 315);
+            this.Controls.Add(this.button);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(194, 174);
+            this.Name = "Form1";
+            this.Text = "Running button";
+            this.ClientSizeChanged += new System.EventHandler(this.Form1_ClientSizeChanged);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnMouseMove);
+            this.ResumeLayout(false);
+
+            }
+
+            #endregion
+
+            private System.Windows.Forms.Button button;
+        }
+    }
+
+
---
+++ button/button/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Button
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Поздравляем! Вы смогли нажать на кнопку!");
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            ///X,Y координаты верхнего левого угла button относительно контейнера
+            ///width,height-size
+            ///margin-значение внешнего поля элемента
+            ///если положение курсора 
+            if (e.X > button.Left - 10 && e.X < button.Left + button.Width + 10)
+            {
+ ///если курсор находится правее или левее кнопки
+                if (e.X >= (button.Left + (button.Width / 2)))
+                    button.Left -= 10;
+                else
+                    button.Left += 10;
+            }
+
+            if (e.Y >= button.Top - 10 && e.Y <= button.Top + button.Height + 10)
+            {
+///если курсор находится ниже или выше кнопки
+                if (e.Y >= (button.Top + (button.Height / 2)))
+                    button.Top -= 10;
+                else
+                    button.Top += 10;
+            }
+
+            ///проверка выходит ли кнопка за границы формы
+            if (button.Left < 0)
+                button.Left = 0;
+            if ((button.Left + button.Width) > this.ClientSize.Width)
+                button.Left = this.ClientSize.Width - button.Width;
+            if (button.Top < 0)
+                button.Top = 0;
+            if ((button.Top + button.Height) > this.ClientSize.Height)
+                button.Top = this.ClientSize.Height - button.Height;
+        }
+
+        private void Form1_ClientSizeChanged(object sender, EventArgs e)
+        {
+            //проверка выходит ли кнопка за границы формы
+            if (button.Left < 0)
+                button.Left = 0;
+            if ((button.Left + button.Width) > this.ClientSize.Width)
+                button.Left = this.ClientSize.Width - button.Width;
+            if (button.Top < 0)
+                button.Top = 0;
+            if ((button.Top + button.Height) > this.ClientSize.Height)
+                button.Top = this.ClientSize.Height - button.Height;
+        }
+    }
+}
